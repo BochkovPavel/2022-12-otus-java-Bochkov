89f0117d256095b9eb4fd1d4120be5c71ae8baeb
Bochkov Pavel <bochkovpv@mail.ru>
2023-01-15 08:18:53 +0300

    HW01-gradle Исправление замечаний.

--- Hw01-gradle/src/main/java/ru/hw/HelloOtus.java
+++ Hw01-gradle/src/main/java/ru/hw/HelloOtus.java
@@ -2,7 +2,7 @@ package ru.hw;
 
 import static com.google.common.base.Strings.commonSuffix;
 
-public class helloOtus {
+public class HelloOtus {
     public static void main(String... args) {
         System.out.println(commonSuffix("Mike", "Coke"));
     }
